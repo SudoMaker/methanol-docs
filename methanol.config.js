b6faef758e383a38e6c3bf0a68b0e101b60aae2f
Yukino Song <nutosservice@gmail.com>
2026-01-17 20:34:56 +0800

    Update methanol for auto code highlighting

--- methanol.config.js
+++ methanol.config.js
@@ -1,15 +1,9 @@
-import { common } from '@wooorm/starry-night'
-import sourceMdx from '@wooorm/starry-night/source.mdx'
-
 import Sitemap from 'vite-plugin-sitemap'
 
 export default ({ mode }) => ({
 	site: {
 		name: 'Methanol Docs',
 		repoBase: 'https://github.com/SudoMaker/methanol-docs/tree/main/pages/'
-	},
-	starryNight: {
-		grammars: [...common, sourceMdx]
 	},
 	pagefind: true,
 	pwa: true,
